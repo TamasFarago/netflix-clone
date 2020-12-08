e16ba4168124a7f2b0992fb0683db6723e56c6ff
Tamas Farago <67281301+TamasFarago@users.noreply.github.com>
2020-12-08 17:25:10 +0100

    finished faq

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, {useState, createContext, useContext} from 'react'
 import "./Accordion.css"
-import faqData from "../../fixtures/faqs.json"
+import faqData from "../../fixtures/faqs"
 
-function Accordion() {
+
+function Accordion(children, ...restProps) {
+    // const { toggleShow, setToggleShow } = useContext(ToggleContext);
+
     return (
         <section className="accordion">
+            <h1>Frequently Asked Questions</h1>
             <div className="accordion__container">
-                <h1>Frequently Asked Questions</h1>
-                {}
+        
+                {faqData.map((item) => (
+                    <AccordionItem key={item.id} {...restProps} body={item.body} header={item.header}>{children}</AccordionItem>
+                ))}
             </div>
         </section>
     )
 }
+
+        const AccordionItem = ({header, body}) => {
+            const [ toggleShow, setToggleShow ] = useState(false);
+            
+            return (
+            <div className="accordion__item">
+                <div className="accoridon__item--header" onClick={() => setToggleShow(!toggleShow)}>{header}</div>
+                <div className={toggleShow ? "accordion__item--body open" : "accordion__item--body"}><p>{body}</p>
+                    </div>
+            </div>
+
+            )
+        }
+
 
 export default Accordion
